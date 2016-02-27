a2f92a7ee8c48a8e1c5cbf1b3165bdc0848a22d6
Frederik Ikemann <fredde@wuaaah.de>
2016-02-27 13:09:01 +0100

    update image path

--- Semester2/Aufgabe1/main.cpp
+++ Semester2/Aufgabe1/main.cpp
@@ -8,9 +8,9 @@ int main() {
     PGMBildRepo *repo = new PGMBildRepo();
     PGMBild *bild     = new PGMBild();
 
-    repo->bild_lesen(bild, "/Users/private/Studium/C++/Piggeldi/dreifach.pgm");
+    repo->bild_lesen(bild, "/Users/private/Studium/C++/Piggeldi/Semester2/Aufgabe1/dreifach.pgm");
 
-    repo->bild_schreiben(bild,"/Users/private/Studium/C++/Piggeldi/dreifach2.pgm");
+    repo->bild_schreiben(bild,"/Users/private/Studium/C++/Piggeldi/Semester2/Aufgabe1/dreifach2.pgm");
 
     repo->delete_pixel_matrix(bild->bild);
 }
